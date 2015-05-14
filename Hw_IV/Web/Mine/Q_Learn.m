a944e29d462a0f2c519cefac8ee8d3d46e75bda7
cemaivaz <cemrifkiaydin@gmail.com>
2015-05-14 06:40:02 +0300

    Comments being written

--- Hw_IV/Web/Mine/Q_Learn.m
+++ Hw_IV/Web/Mine/Q_Learn.m
@@ -1,33 +1,26 @@
-
-% CliffWalkingQLearning: implements the cliff-walking problem using the
-% Q-Learning method
-
-% You can pass the parameters of the problem, through the options
-% structure, otherwise a default settings is used for running the program.
-
-% written by: Sina Iravanian - June 2009
-% sina@sinairv.com
-% Please send your comments or bug reports to the above email address.
+% Q-Learning
+%HW-IV
 
 close all;
 clear all;
 clc
 
-gamma = 0.9;
 alpha = 0.05;
+%The below epsilon value decays throughout episodes
 epsilon = 0.1;
-gridcols = 8;
+gamma = 0.9;
+
+%The number of rows, and columns, respectively
 gridrows = 8;
-fontsize = 16;
-showTitle = 1;
+gridcols = 8;
 
-episodeCount = 2000;
-selectedEpisodes = [20 200 700 1000 2000];
-isKing = 0;
-canHold = 0;
-
+%The starting point: (1,1) - The lowest left corner
+%The goal: (7, 6)
 startrow = 1;
 goalrow = 7;
+
+%N: North, W: West, S: South, E: East
+actionCount = 4;
 
 start.row = gridrows - startrow + 1;
 start.col = 1;
@@ -35,78 +28,76 @@ goal.row = gridrows - goalrow + 1;
 goal.col = 6;
 
 
-selectedEpIndex = 1;
-if(isKing ~= 0),  actionCount = 8; else actionCount = 4; end
-if(canHold ~= 0 && isKing ~= 0), actionCount = actionCount + 1; end
-
 % initialize Q with zeros
 Q = rand(gridrows, gridcols, actionCount);
 
+
+
 a = 0; % an invalid action
-% loop through episodes
+episodeCount = 1900;
+
+%Episodes
 for ei = 1:episodeCount,
-    %disp(sprintf('Running episode %d', ei));
+    
     curpos = start;
     nextpos = start;
     
  
     
     epsilon = 0.9;
-    %epsilon or greedy
-    if(rand > epsilon) % greedy
+    %e-Greedy method
+    if(rand > epsilon)
         [qmax, a] = max(Q(curpos.row,curpos.col,:));
     else
         a = IntRand(1, actionCount);
     end
 
-
-    %epsilon = epsilon * 0.975;
     episodeFinished = 0;
     while(episodeFinished == 0)
        
-        % take action a, observe r, and nextpos
-        nextpos = GiveNextPos(curpos, a, gridcols, gridrows);
+        
+        nextpos = GiveNextPos(curpos, a, gridcols, gridrows); %The action "a" is
+        %taken, and the next state (position) is returned
+        
         if(PosCmp(nextpos, goal) == 0), 
             episodeFinished = 1;
-            r = 100; 
-%         elseif(nextpos.row == gridrows && 1 < nextpos.col && nextpos.col < gridcols) 
-%             episodeFinished = 1;
-%             r = 0; 
+            %r: Reward
+            %Gaussian with mean 100 and stdev 10
+            r = normrnd(100, 10); 
         else
             r = 0;
         end
 
-        % choose a_next from nextpos
-        [qmax, a_next] = max(Q(nextpos.row,nextpos.col,:));
-        if(rand <= epsilon) % greedy
+        
+        [qmax, a_next] = max(Q(nextpos.row,nextpos.col,:)); %In whichever direction
+        %the maximum value is produced, the corresponding state, and the
+        %value thereof are returned
+        
+        %e-Greedy
+        if(rand <= epsilon)
             a_next = IntRand(1, actionCount);
         end
         
         
-        % update Q: Sarsa
+        
         curQ = Q(curpos.row, curpos.col, a);
-        nextQ = qmax; %Q(nextpos.row, nextpos.col, a_next);
-%         Q(curpos.row, curpos.col, a) = curQ + alpha*(r + gamma*nextQ - curQ);
-        
-        
+        nextQ = qmax;
+
+        %The next state when action is performed
         destPos = GiveNextPos(curpos, a_next, gridcols, gridrows);
         
 
+        %Two orthogonal states
         [orthogSt1, orthogSt2] = ProbPos(curpos, destPos, gridrows);
-        
-%         fprintf('___');
-%         curpos
-%         a_next
-%         destPos
-%         fprintf('//////');
-%         orthogSt1
-%         
-%         orthogSt2
-        
+
+        %The below variable is used in choosing which way of three
+        %directions to take
         directRand = rand;
         [orthogVal1, a_next_ort1] = max(Q(orthogSt1.row,orthogSt1.col,:));
         [orthogVal2, a_next_ort2] = max(Q(orthogSt2.row,orthogSt2.col,:));
         
+        %With the greatest probability, it chooses the intended state, with
+        %less probabilities, one of the orthogonal directions
         if directRand <= 0.5
             Q(curpos.row, curpos.col, a) = (curQ + alpha*(r + gamma*nextQ - curQ));
             curpos = nextpos;
@@ -121,46 +112,16 @@ for ei = 1:episodeCount,
             a = a_next_ort2;
         end
         
-
-        
-%         Q(curpos.row, curpos.col, a) = .5 * (curQ + alpha*(r + gamma*nextQ - curQ));
-%         Q(curpos.row, curpos.col, a) = Q(curpos.row, curpos.col, a) + .25 * (curQ + alpha*(r + gamma*orthogVal1 - curQ));
-%         Q(curpos.row, curpos.col, a) = Q(curpos.row, curpos.col, a) + .25 * (curQ + alpha*(r + gamma*orthogVal2 - curQ));
-        
-
-%         curpos = nextpos;
-%         a = a_next;
+        %The epsilon value decays
         epsilon = epsilon * 0.922;
-    end % states in each episode
+    end
     
-    % if the current state of the world is going to be drawn ...
-%     if(selectedEpIndex <= length(selectedEpisodes) && ei == selectedEpisodes(selectedEpIndex))
-%         curpos = start;
-%         rows = []; cols = []; acts = [];
-%         for i = 1:(gridrows + gridcols) * 10,
-%             [qmax, a] = max(Q(curpos.row,curpos.col,:));
-%             nextpos = GiveNextPos(curpos, a, gridcols, gridrows);
-%             rows = [rows curpos.row];
-%             cols = [cols curpos.col];
-%             acts = [acts a];
-% 
-%             if(PosCmp(nextpos, goal) == 0), break; end
-%             curpos = nextpos;
-%         end % states in each episode
-%         
-%         %figure;
-%         figure('Name',sprintf('Episode: %d', ei), 'NumberTitle','off');
-%         DrawCliffEpisodeState(rows, cols, acts, start.row, start.col, goal.row, goal.col, gridrows, gridcols, fontsize);
-%         if(showTitle == 1),
-%             title(sprintf('Cliff Walking Q-Learning - episode %d - (\\epsilon: %3.3f), (\\alpha = %3.4f), (\\gamma = %1.1f)', ei, epsilon, alpha, gamma));
-%         end
-%         
-%         selectedEpIndex = selectedEpIndex + 1;
-%     end
 
-end % episodes loop
+end
 
 
+%Below are the corresponding variables showing the optimal actions, and the
+%scores thereof
 [max_, ind_] = max(Q, [], 3);
 fprintf('Max_a Q(s, a):\n');
 max_
@@ -183,7 +144,7 @@ for i = 1:size(ind_, 1)
         end
     end
 end
-%Bump into the wall? HOLD!
+%Bump into the wall? Hold.
 indDir
 
 
